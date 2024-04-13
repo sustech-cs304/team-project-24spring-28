517d4b832b1ccb83f79b43d0c936728b94a5df0b
LampTales <179500516@qq.com>
2024-04-14 00:48:16 +0800

    event page built some bugs of avatar needed to be fixed

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,10 +5,16 @@ import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import VueMarkdownEditor from '@kangc/v-md-editor';
+import VMdPreview from '@kangc/v-md-editor/lib/preview';
 import '@kangc/v-md-editor/lib/style/base-editor.css';
+
 import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
 import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
 import Prism from 'prismjs';
+
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+import hljs from 'highlight.js';
 
 import App from './App.vue'
 
@@ -39,6 +45,10 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 }
 
 VueMarkdownEditor.use(vuepressTheme, {
+    Prism,
+});
+
+VMdPreview.use(vuepressTheme, {
     Prism,
 });
 
