6a6755f4b5c2e1a7f21462c01a39e13afc78158c
LampTales <179500516@qq.com>
2024-04-14 18:17:25 +0800

    finish the page CreateEvent.vue

--- frontend/src/components/User/pages/event/CreateEvent.vue
+++ frontend/src/components/User/pages/event/CreateEvent.vue
@@ -1,36 +1,80 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import VMdEditor from '@kangc/v-md-editor'
 
-let form = ref({
+let mdText = ref('')
+
+const form = reactive({
   title: '',
-  description: '',
-  location: '',
+  name: '',
+  type: '',
+  applyStartTime: '',
+  applyEndTime: '',
   startTime: '',
   endTime: ''
 })
 
-let mdText = ref('')
-
-let rules = ref({
+const rules = reactive({
   title: [
-    { required: true, message: 'Please input the title', trigger: 'blur' }
+    { required: true, message: '请输入活动标题', trigger: 'blur' }
   ],
-  description: [
-    { required: true, message: 'Please input the description', trigger: 'blur' }
+  name: [
+    { required: true, message: '请输入活动名', trigger: 'blur' }
   ],
-  location: [
-    { required: true, message: 'Please input the location', trigger: 'blur' }
+  type: [
+    { required: true, message: '请选择活动类型', trigger: 'change' }
+  ],
+  applyStartTime: [
+    { required: true, message: '请选择报名开始时间', trigger: 'change' }
+  ],
+  applyEndTime: [
+    { required: true, message: '请选择报名结束时间', trigger: 'change' }
   ],
   startTime: [
-    { required: true, message: 'Please input the start time', trigger: 'blur' }
+    { required: true, message: '请选择活动开始时间', trigger: 'change' }
   ],
   endTime: [
-    { required: true, message: 'Please input the end time', trigger: 'blur' }
+    { required: true, message: '请选择活动结束时间', trigger: 'change' }
   ]
 })
 
-mdText = 'sdf'
+let eventTypes = [
+  { value: '1', label: '计数式' },
+  { value: '2', label: '选座式' },
+  { value: '3', label: '自定义报名' },
+]
+
+let pickerOptions = {
+  shortcuts: [
+    {
+      text: '今天',
+      onClick(picker) {
+        picker.$emit('pick', new Date())
+      }
+    },
+    {
+      text: '昨天',
+      onClick(picker) {
+        const date = new Date()
+        date.setTime(date.getTime() - 3600 * 1000 * 24)
+        picker.$emit('pick', date)
+      }
+    },
+    {
+      text: '一周前',
+      onClick(picker) {
+        const date = new Date()
+        date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+        picker.$emit('pick', date)
+      }
+    }
+  ]
+}
+
+mdText = 'sdf\n' +
+    '### Title\n' +
+    '\n' +
+    '![Description](https://github.com/LampTales/YuxiaLin/raw/main/pics/lin.jpg){{{width="200" height="auto"}}}'
 
 </script>
 
@@ -38,45 +82,66 @@ mdText = 'sdf'
   <div>
     <p>Header</p>
   </div>
-  <div>
+  <div style="overflow-y: scroll; height: 90vh">
     <div>
-      <el-form :model="form" :rules="rules" ref="form" label-width="120px">
-        <el-form-item label="Title" prop="title">
+      <el-form :model="form" :rules="rules" label-width="120px">
+        <el-form-item label="活动标题" prop="title" style="width: 600px">
           <el-input v-model="form.title"></el-input>
         </el-form-item>
-        <el-form-item label="Description" prop="description">
-          <el-input v-model="form.description"></el-input>
+        <el-form-item label="活动名" prop="name" style="width: 600px">
+          <el-input v-model="form.name"></el-input>
         </el-form-item>
-        <el-form-item label="Location" prop="location">
-          <el-input v-model="form.location"></el-input>
+        <el-form-item label="活动类型" prop="type">
+          <el-select v-model="form.type" placeholder="请选择活动类型">
+            <el-option v-for="item in eventTypes" :key="item.value" :label="item.label" :value="item.value"/>
+          </el-select>
         </el-form-item>
-        <el-form-item label="Start Time" prop="startTime">
+        <el-form-item label="报名开始时间" prop="applyStartTime">
+          <el-date-picker
+            v-model="form.applyStartTime"
+            type="datetime"
+            placeholder="选择日期和时间"
+            :picker-options="pickerOptions">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="报名结束时间" prop="applyEndTime">
+          <el-date-picker
+            v-model="form.applyEndTime"
+            type="datetime"
+            placeholder="选择日期和时间"
+            :picker-options="pickerOptions">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="活动开始时间" prop="startTime">
           <el-date-picker
             v-model="form.startTime"
             type="datetime"
-            placeholder="Pick a date and time"
+            placeholder="选择日期和时间"
             :picker-options="pickerOptions">
           </el-date-picker>
         </el-form-item>
-        <el-form-item label="End Time" prop="endTime">
+        <el-form-item label="活动结束时间" prop="endTime">
           <el-date-picker
             v-model="form.endTime"
             type="datetime"
-            placeholder="Pick a date and time"
+            placeholder="选择日期和时间"
             :picker-options="pickerOptions">
           </el-date-picker>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="onSubmit">Create</el-button>
-          <el-button @click="onCancel">Cancel</el-button>
         </el-form-item>
       </el-form>
     </div>
 
     <div>
+      <p style="margin-left: 25px; font-size: 14px; color: #606266"
+      >活动正文</p>
+    </div>
+    <div style="width: 90%; display: flex; flex-direction: row; justify-content: center; margin-left: 50px">
       <v-md-editor v-model="mdText"></v-md-editor>
     </div>
 
+    <div style="margin-top: 30px; display: flex; flex-direction: row; justify-content: center;">
+      <el-button type="primary">发起活动</el-button>
+    </div>
   </div>
 </template>
 
