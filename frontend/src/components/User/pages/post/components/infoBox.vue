1d1d2be738abf38e8eb35be26424f6654606d262
JikoSchnee <104906844+JikoSchnee@users.noreply.github.com>
2024-04-20 20:48:03 +0800

    PostSquare v2.1.4
    更改了帖子卡片的样式

---
+++ frontend/src/components/User/pages/post/components/infoBox.vue
@@ -0,0 +1,65 @@
+<script setup>
+import {ref, onMounted, onBeforeUnmount, defineProps} from 'vue';
+
+const props = defineProps({
+    background: {
+        type: String,
+        default: 'C:\\Users\\27449\\Desktop\\personalProject\\team-project-24spring-28\\frontend\\src\\assets\\Like\\like.png'
+    },
+    givenNumber: {
+        type: Number,
+        default: '1234'
+    }
+
+});
+const num = ref(null);
+const adjustFontSize = () => {
+    const NUM = parseInt(num.value.innerText);
+    if (NUM > 9999) {
+        num.value.innerText = '9999+';
+    } else {
+        const length = NUM.toString().length;
+        if (length <= 2) {
+            num.value.style.fontSize = '40px';
+        } else if (length <= 3) {
+            num.value.style.fontSize = '30px';
+        } else if (length <= 4) {
+            num.value.style.fontSize = '20px';
+        }
+    }
+};
+
+onMounted(() => {
+    adjustFontSize();
+    window.addEventListener('resize', adjustFontSize);
+});
+
+onBeforeUnmount(() => {
+    window.removeEventListener('resize', adjustFontSize);
+});
+</script>
+
+<template>
+    <div class="box" :style="{ backgroundImage: `url(${props.background})` }">
+        <span ref="num" class="number">{{props.givenNumber}}</span>
+    </div>
+</template>
+
+<style scoped>
+.box {
+    height: 8vh;
+    //background-color: #ccc;
+    margin: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    border-radius: 10px;
+    background-size: auto;
+}
+.number {
+    position: absolute;
+    color: white;
+    font-family: 'Arial Rounded MT Bold', sans-serif;
+}
+</style>
--- frontend/src/components/User/pages/post/components/postsGround/postCard.vue
+++ frontend/src/components/User/pages/post/components/postsGround/postCard.vue
@@ -2,6 +2,8 @@
 import { Pointer, Share, StarFilled } from "@element-plus/icons";
 import { ChatDotSquare } from "@element-plus/icons-vue";
 import { useRouter } from "vue-router";
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import InfoBox from "@/components/User/pages/post/components/infoBox.vue";
 
 const router = useRouter();
 
@@ -10,6 +12,39 @@ const PointerIcon = Pointer;
 const ShareIcon = Share;
 const StarFilledIcon = StarFilled;
 
+
+// const number1 = ref(null);
+// const number2 = ref(null);
+// const number3 = ref(null);
+// const number4 = ref(null);
+//
+// const adjustFontSize = () => {
+//     const numbers = [number1.value, number2.value, number3.value, number4.value];
+//     numbers.forEach(number => {
+//         const num = parseInt(number.innerText);
+//         if (num > 9999) {
+//             number.innerText = '9999+';
+//         } else {
+//             const length = num.toString().length;
+//             if (length <= 2) {
+//                 number.style.fontSize = '40px';
+//             } else if (length <= 3) {
+//                 number.style.fontSize = '30px';
+//             } else if (length <= 4) {
+//                 number.style.fontSize = '20px';
+//             }
+//         }
+//     });
+// };
+
+// onMounted(() => {
+//     adjustFontSize();
+//     window.addEventListener('resize', adjustFontSize);
+// });
+//
+// onBeforeUnmount(() => {
+//     window.removeEventListener('resize', adjustFontSize);
+// });
 function goToPost() {
     // router.push({ path: '/square/post' });
 
@@ -56,56 +91,77 @@ function goToPost() {
                 </el-col>
                 <el-col :span="4">
                     <el-row>
-                        <el-col style="display: flex; flex-direction: column; justify-content: center;">
-                            <el-row style="margin-bottom: 5px">
-                                <el-button-group>
-                                    <el-button type="primary" :icon="Pointer" class="button-left">
-                                        点赞
-                                    </el-button>
-                                    <el-button type="primary" plain  class="button-right">
-                                        10
-                                    </el-button>
-                                </el-button-group>
-                            </el-row>
-                            <el-row style="margin-bottom: 5px">
-                                <el-button-group>
-                                    <el-button type="primary" :icon="StarFilled" class="button-left">
-                                        收藏
-                                    </el-button>
-                                    <el-button type="primary" plain class="button-right">
-                                        29
-                                    </el-button>
-                                </el-button-group>
-                            </el-row>
-                            <el-row style="margin-bottom: 5px">
-                                <el-button-group>
-                                    <el-button type="primary" :icon="Share" class="button-left">
-                                        分享
-                                    </el-button>
-                                    <el-button type="primary" plain class="button-right">
-                                        7
-                                    </el-button>
-                                </el-button-group>
-                            </el-row>
-                            <el-row >
-                                <el-button-group>
-                                    <el-button type="primary" :icon="ChatDotSquare" class="button-left">
-                                        评论
-                                    </el-button>
-                                    <el-button type="primary" plain class="button-right">
-                                        20
-                                    </el-button>
-                                </el-button-group>
-                            </el-row>
+                        <el-col :span="12">
+<!--                            <div class="box">-->
+<!--                                <span ref="number1" class="number">12</span>-->
+<!--                            </div>-->
+                            <info-box :given-number="1" :background="'@/src/assets/Like/like.png'"></info-box>
                         </el-col>
-
+                        <el-col :span="12">
+<!--                            <div class="box">-->
+<!--                                <span ref="number2" class="number">333</span>-->
+<!--                            </div>-->
+                            <info-box></info-box>
+                        </el-col>
+                        <el-col :span="12">
+<!--                            <div class="box">-->
+<!--                                <span ref="number3" class="number">4444</span>-->
+<!--                            </div>-->
+                            <info-box :given-number="32456"></info-box>
+                        </el-col>
+                        <el-col :span="12">
+<!--                            <div class="box">-->
+<!--                                <span ref="number4" class="number">5555555555</span>-->
+<!--                            </div>-->
+                            <info-box></info-box>
+                        </el-col>
                     </el-row>
-                    <el-row>
+<!--                    <el-row>-->
+<!--                        <el-col style="display: flex; flex-direction: column; justify-content: center;">-->
+<!--                            <el-row style="margin-bottom: 5px">-->
+<!--                                <el-button-group>-->
+<!--                                    <el-button type="primary" :icon="Pointer" class="button-left">-->
+<!--                                        点赞-->
+<!--                                    </el-button>-->
+<!--                                    <el-button type="primary" plain  class="button-right">-->
+<!--                                        10-->
+<!--                                    </el-button>-->
+<!--                                </el-button-group>-->
+<!--                            </el-row>-->
+<!--                            <el-row style="margin-bottom: 5px">-->
+<!--                                <el-button-group>-->
+<!--                                    <el-button type="primary" :icon="StarFilled" class="button-left">-->
+<!--                                        收藏-->
+<!--                                    </el-button>-->
+<!--                                    <el-button type="primary" plain class="button-right">-->
+<!--                                        29-->
+<!--                                    </el-button>-->
+<!--                                </el-button-group>-->
+<!--                            </el-row>-->
+<!--                            <el-row style="margin-bottom: 5px">-->
+<!--                                <el-button-group>-->
+<!--                                    <el-button type="primary" :icon="Share" class="button-left">-->
+<!--                                        分享-->
+<!--                                    </el-button>-->
+<!--                                    <el-button type="primary" plain class="button-right">-->
+<!--                                        7-->
+<!--                                    </el-button>-->
+<!--                                </el-button-group>-->
+<!--                            </el-row>-->
+<!--                            <el-row >-->
+<!--                                <el-button-group>-->
+<!--                                    <el-button type="primary" :icon="ChatDotSquare" class="button-left">-->
+<!--                                        评论-->
+<!--                                    </el-button>-->
+<!--                                    <el-button type="primary" plain class="button-right">-->
+<!--                                        20-->
+<!--                                    </el-button>-->
+<!--                                </el-button-group>-->
+<!--                            </el-row>-->
+<!--                        </el-col>-->
 
-                    </el-row>
-                    <el-row>
+<!--                    </el-row>-->
 
-                    </el-row>
                 </el-col>
             </el-row>
         </el-card>
@@ -136,5 +192,18 @@ function goToPost() {
     font-weight: bold;
     font-size: 20px;
 }
-
+.box {
+    height: 8vh;
+    background-color: #ccc;
+    margin: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+}
+.number {
+    position: absolute;
+    color: white;
+    font-family: 'Arial Rounded MT Bold', sans-serif;
+}
 </style>
