40db7294abdd1453cc56c6cb7dd7eacf31c7dd1c
Shinomiya <2580463266@qq.com>
2024-04-25 18:26:26 +0800

    Implement Message module and add attribute "name" to AbstractUser

--- backend/src/main/java/org/example/backend/dto/MessageDto.java
+++ backend/src/main/java/org/example/backend/dto/MessageDto.java
@@ -1,10 +1,17 @@
 package org.example.backend.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class MessageDto {
     Long Id;
     String type;
     Long to;
     Long from;
+    String toName;
+    String fromName;
     Boolean read;
     String time;
     String content;
@@ -12,59 +19,4 @@ public class MessageDto {
     public MessageDto() {
     }
 
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Long getTo() {
-        return to;
-    }
-
-    public void setTo(Long to) {
-        this.to = to;
-    }
-
-    public Long getFrom() {
-        return from;
-    }
-
-    public void setFrom(Long from) {
-        this.from = from;
-    }
-
-    public Boolean getRead() {
-        return read;
-    }
-
-    public void setRead(Boolean read) {
-        this.read = read;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
