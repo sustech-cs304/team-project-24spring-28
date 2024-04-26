0784e84e357a0e6b384cfff2425647aa9d916877
Wang <wyr321321@126.com>
2024-04-26 16:10:56 +0800

    comment

---
+++ backend/src/main/java/org/example/backend/app/CommentApp.java
@@ -0,0 +1,12 @@
+package org.example.backend.app;
+
+import org.example.backend.service.EventCommentService;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/comment")
+public class CommentApp {
+    private final EventCommentService eventCommentService;
+    private final
+}
