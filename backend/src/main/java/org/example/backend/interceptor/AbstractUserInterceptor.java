70a4d7642fecf0f5adbbba8613c4dc01882b4be9
wangyr <12110330@mail.sustech.edu.cn>
2024-06-01 00:29:18 +0800

    disable Interceptor

--- backend/src/main/java/org/example/backend/interceptor/AbstractUserInterceptor.java
+++ backend/src/main/java/org/example/backend/interceptor/AbstractUserInterceptor.java
@@ -9,20 +9,20 @@ import org.example.backend.config.MyException;
 
 public class AbstractUserInterceptor implements HandlerInterceptor {
 
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        String token = request.getHeader("Authorization");
-//        if (token == null) {
-//            System.out.println(request.getRequestURL());
-//            System.out.println("token is null");
-//            throw new MyException(1, "token is null");
-//        }
-//        AbstractUser abstractUser = JwtUtil.verifyToken(token);
-//        if (abstractUser == null) {
-//            System.out.println(request.getRequestURL());
-//            System.out.println("token is invalid");
-//            throw new MyException(2, "token is invalid");
-//        }
-//        return true;
-//    }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("Authorization");
+        if (token == null) {
+            System.out.println(request.getRequestURL());
+            System.out.println("token is null");
+            throw new MyException(1, "token is null");
+        }
+        AbstractUser abstractUser = JwtUtil.verifyToken(token);
+        if (abstractUser == null) {
+            System.out.println(request.getRequestURL());
+            System.out.println("token is invalid");
+            throw new MyException(2, "token is invalid");
+        }
+        return true;
+    }
 }
