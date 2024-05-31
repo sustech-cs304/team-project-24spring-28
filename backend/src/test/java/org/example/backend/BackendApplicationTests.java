6724261aa94a8ca11f856a807fa98bb4a6e9050e
Shinomiya <2580463266@qq.com>
2024-05-31 23:25:55 +0800

    Edit for test

--- backend/src/test/java/org/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/example/backend/BackendApplicationTests.java
@@ -3,11 +3,13 @@ package org.example.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
 // This will not start the server, but will start the application context
 //@SpringBootTest()
 // This will start the server
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class BackendApplicationTests {
     // This will inject the port that the server is running on
     @LocalServerPort
