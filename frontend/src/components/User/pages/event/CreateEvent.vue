cfb4982631e6ec69eca4021e9d885289e3420f74
LampTales <179500516@qq.com>
2024-06-02 04:25:52 +0800

    clean comments

--- frontend/src/components/User/pages/event/CreateEvent.vue
+++ frontend/src/components/User/pages/event/CreateEvent.vue
@@ -261,33 +261,6 @@ function formatTime(str) {
 }
 
 function createEventClick() {
-  // console.log(form)
-  // console.log(mdText)
-  // let temp = new FormData()
-  // temp.append('title', form.title)
-  // temp.append('name', form.name)
-  // temp.append('applyStartTime', formatTime(form.applyStartTime))
-  // temp.append('applyEndTime', formatTime(form.applyEndTime))
-  // temp.append('startTime', formatTime(form.startTime))
-  // temp.append('endTime', formatTime(form.endTime))
-  // temp.append('introduction', form.introduction)
-  // temp.append('imageUrl', imageUrl.value)
-  // temp.append('mdText', mdText.value)
-  // if (form.type === '1') {
-  //   temp.append('enrollmentType', 'count')
-  //   if (form.limitCount === '') {
-  //     temp.append('limitCount', 0)
-  //   } else {
-  //     temp.append('limitCount', Number(form.limitCount))
-  //   }
-  // } else if (form.type === '2') {
-  //   temp.append('enrollmentType', 'select')
-  // } else {
-  //   temp.append('enrollmentType', 'form')
-  //   temp.append('limitCount', 0)
-  //   temp.append('definedForm', JSON.stringify(definedForm.value))
-  // }
-  // change to JSON string
   let temp = {
     title: form.title,
     name: form.name,
@@ -315,9 +288,8 @@ function createEventClick() {
     temp.definedForm = definedForm.value
   }
   let jsonContent = JSON.stringify(temp)
-  axiosInstance.post('/event/create', {
-    content: jsonContent
-  }).then((res) => {
+  // jsonContent = "{\"title\": \"asdf\"}"
+  axiosInstance.post('/event/create', jsonContent).then((res) => {
     console.log(res)
   }).catch((err) => {
     console.log(err)
