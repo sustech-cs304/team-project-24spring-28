20f55fa42cbd71d027a018e5904a2f81cbfd5a4f
Agouiscoding <105056924+Agouiscoding@users.noreply.github.com>
2024-06-02 09:24:42 +0800

    sighUp optimization

--- frontend/src/components/User/pages/main/signUp.vue
+++ frontend/src/components/User/pages/main/signUp.vue
@@ -3,12 +3,15 @@ import  {reactive, ref} from "vue";
 import axiosInstance from "@/utils/axios";
 import {useRouter} from "vue-router";
 
-
+const router = useRouter()
 const registerForm = reactive({
   username: '',
   password: ''
 })
 
+function toLogin() {
+  router.push({path: '/'})
+}
 
 
 const rules = reactive({
@@ -81,6 +84,12 @@ const register = () => {
         <el-input v-model="registerForm.password" show-password style="max-width: 300px"></el-input>
       </el-form-item>
     </el-form>
+
+    <div @click="toLogin">
+      <p class="signUpText" style="margin-left: 110px; margin-top: 10px; color: #6bccff; cursor: pointer">
+        Go back to login
+      </p>
+    </div>
 
     <button type="submit" class="submit" @click="register">
       Sign up
